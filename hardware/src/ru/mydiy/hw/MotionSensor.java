a10e90dfeef6f8cc19df96a8d8b9652aedfbec28
Алексей <guha5277@yandex.ru>
2020-02-04 20:44:02 +0700

    Рефакторинг года. Добавление комментариев.

--- hardware/src/ru/mydiy/hw/MotionSensor.java
+++ hardware/src/ru/mydiy/hw/MotionSensor.java
@@ -14,34 +14,25 @@ public class MotionSensor implements GpioPinListenerDigital {
     private final String name;
     private final boolean HIGH = true;
     private final boolean LOW = false;
-    private final int GPIO_00 = 0;
-    private final int GPIO_01 = 1;
-    private static final Pin[] orangePins = {OrangePiPin.GPIO_01, OrangePiPin.GPIO_00};
+    private static final Pin[] orangePins = {OrangePiPin.GPIO_00, OrangePiPin.GPIO_01, OrangePiPin.GPIO_02, OrangePiPin.GPIO_03, OrangePiPin.GPIO_04};
     private TimeChecker timeChecker;
     private long activityTime;
     private long activitySum;
     private long timeOfLastActivity;
 
-    public MotionSensor(MotionSensorListener listener, String name){
+    public MotionSensor(MotionSensorListener listener, String name, int gpioNumber){
+        this.name = name;
+        this.listener = listener;
+        //Инициализация платформы, настройка GPIO
         try {
             PlatformManager.setPlatform(Platform.ORANGEPI);
         } catch (PlatformAlreadyAssignedException e){
-
+            listener.onSensorException(this, e);
         }
         gpio = GpioFactory.getInstance();
-        this.name = name;
-        this.listener = listener;
-        inputPin = gpio.provisionDigitalInputPin(orangePins[GPIO_01], name, PinPullResistance.PULL_DOWN);
+        inputPin = gpio.provisionDigitalInputPin(orangePins[gpioNumber], name, PinPullResistance.PULL_DOWN);
         inputPin.addListener(this);
         inputPin.setShutdownOptions(true);
-    }
-
-    public void activate(){
-        inputPin.addListener(this);
-    }
-
-    public void deactivate(){
-        inputPin.removeAllListeners();
     }
 
     public String getName(){
@@ -52,7 +43,6 @@ public class MotionSensor implements GpioPinListenerDigital {
         return System.currentTimeMillis() - timeOfLastActivity;
     }
 
-
     private MotionSensor getMotionSensor(){
         return this;
     }
@@ -61,6 +51,7 @@ public class MotionSensor implements GpioPinListenerDigital {
         return activitySum;
     }
 
+    //Обработка событий изменения состояния GPIO
     @Override
     public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent gpioPinDigitalStateChangeEvent) {
         PinState state = gpioPinDigitalStateChangeEvent.getState();
@@ -68,32 +59,32 @@ public class MotionSensor implements GpioPinListenerDigital {
             activityTime = System.currentTimeMillis();
             timeOfLastActivity = activityTime;
             listener.motionState(this, HIGH);
+        } else {
+            activityTime = System.currentTimeMillis() - activityTime;
+            activitySum += activityTime;
+            listener.motionState(this, LOW);
+
             if(timeChecker == null || !timeChecker.isAlive()){
                 timeChecker = new TimeChecker();
                 timeChecker.start();
             }
-        } else {
-            listener.motionState(this, LOW);
-            activityTime = System.currentTimeMillis() - activityTime;
-            activitySum += activityTime;
         }
     }
 
+    //Анализирование времени срабатывания, для оценки уровня опасности - есть она или уже нет
     private class TimeChecker extends Thread{
-
-
         @Override
         public void run() {
             listener.debugMessage(getMotionSensor(), "THREAD TimeChecker started!");
             while (!isInterrupted()) {
-                if (inputPin.isLow() && getLastActivityTime() > 30000) {
+                if (inputPin.isLow() && getLastActivityTime() > 35000) {
                         listener.debugMessage(getMotionSensor(), "TimeChecker: ACTIVITY GONE!");
                         listener.activityIsGone(getMotionSensor(), (int) (getActivitySum() / 1000));
                         interrupt();
                 } else {
                     try {
                         listener.debugMessage(getMotionSensor(), "TimeChecker: ACTIVITY IS NOT GONE! OVERALL TIME IS: " + (getActivitySum() / 1000) + ", System time is: " + (System.currentTimeMillis() / 1000));
-                        sleep(5000);
+                        sleep(15000);
                     } catch (InterruptedException e) {
                         listener.onSensorException(getMotionSensor(), e);
                     }
